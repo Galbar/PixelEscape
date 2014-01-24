620e373dbe13e8ccbc5e1d254e9a9a11756407b2
David <david@david-HP-Pavilion-dv6-Notebook-PC.(none)>
2014-01-24 21:28:33 +0100

    added event for exiting the window

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,26 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(512, 512, 32), "Project RGB", sf::Style::Default);
+    sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(512, 512, 32), "Project RGB", sf::Style::Default);
 
     while(1)
     {
+        // EVENTS
+        sf::Event event;
+        if (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window->close();
+                break;
+            }
+            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+            {
+                window->close();
+                break;
+            }
+        }
+
         // HERE LOGIC UPDATES
 
 
@@ -17,6 +33,6 @@ int main()
 
 
 
-        window.display();
+        window->display();
     }
 }
