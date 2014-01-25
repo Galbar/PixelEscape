0b6eddeeb40507aa3838468f6a2e6a4fc05bd670
Sorington <dbalaghi10@gmail.com>
2014-01-25 17:18:50 +0100

    added fast walk

--- SOURCES/Game.cpp
+++ SOURCES/Game.cpp
@@ -3,7 +3,7 @@
 Game::Game(sf::RenderWindow* window)
 {
     m_window = window;
-    m_lvl_paths.push_back("data/levels/level1.png");
+    m_lvl_paths.push_back("data/levels/test.png");
 
     sc = new GameScene(m_window, 1, m_lvl_paths[0]);
 
