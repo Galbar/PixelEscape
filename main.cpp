6fc8a0adc18ecf8265606a7ddce3c03cf2fdf74f
enriqueto <enrique@ravalmatic.com>
2014-01-25 16:20:59 +0100

    puntero statico a window otra vez

--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@
 
 #define WINDOW_SIZE 688 // CAMBIAR TAMBIEN EN GAMESCENE
 
+static sf::RenderWindow *s_window;
+
 int main()
 {
     sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(WINDOW_SIZE, WINDOW_SIZE, 32), "Project RGB", sf::Style::Default);
+
+    s_window = window;
 
     Game game(window);
 
